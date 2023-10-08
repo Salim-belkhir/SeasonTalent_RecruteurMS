4ca65a2ec4db57705527d5826b79c30d257a3538
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-10-08 19:22:32 +0200

    creation of the micro service

---
+++ src/main/java/fr/polytech/Recruteur/RecruteurApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.Recruteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruteurApplication.class, args);
+	}
+
+}
