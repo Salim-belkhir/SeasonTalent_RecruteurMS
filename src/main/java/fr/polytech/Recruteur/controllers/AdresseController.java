a2c242d08f9fd57462da0de4cb8db67350c0b8fd
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-11-08 23:07:18 +0100

    Some correction on routes for adresse

--- src/main/java/fr/polytech/Recruteur/controllers/AdresseController.java
+++ src/main/java/fr/polytech/Recruteur/controllers/AdresseController.java
@@ -3,11 +3,18 @@ package fr.polytech.Recruteur.controllers;
 
 import fr.polytech.Recruteur.models.Adresse;
 import fr.polytech.Recruteur.services.AdresseService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +27,45 @@ public class AdresseController {
     @GetMapping("/{id}")
     public ResponseEntity<Adresse> getAdresseById(@PathVariable Long id) {
         Adresse adresse = adresseService.findAdresseById(id);
-        if (adresse == null) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-        else{
-            return new ResponseEntity<>(adresse, HttpStatus.OK);
-        }
+        HttpStatus status = (adresse == null) ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(adresse, status);
+    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<Adresse>> getAllAdresses() {
+        List<Adresse> adresses = adresseService.findAllAdresses();
+        HttpStatus status = (adresses.isEmpty()) ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(adresses, status);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Adresse> addAdresse(Adresse adresse) {
+        Adresse newAdresse = adresseService.addAdresse(adresse);
+        HttpStatus status = (newAdresse == null) ? HttpStatus.BAD_REQUEST : HttpStatus.CREATED;
+        return new ResponseEntity<>(newAdresse, status);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Adresse> updateAdresse(@PathVariable Long idAdresse, Adresse adresse) {
+        Adresse newAdresse = adresseService.updateAdresse(idAdresse, adresse);
+        HttpStatus status = (newAdresse == null) ? HttpStatus.BAD_REQUEST : HttpStatus.CREATED;
+        return new ResponseEntity<>(newAdresse, status);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Adresse> patchAdresse(@PathVariable Long idAdresse, Adresse adresse) {
+        Adresse newAdresse = adresseService.updateAdresse(idAdresse, adresse);
+        HttpStatus status = (newAdresse == null) ? HttpStatus.BAD_REQUEST : HttpStatus.CREATED;
+        return new ResponseEntity<>(newAdresse, status);
+    }
+
+    //TODO: Gerer ici le cas de si l'utilisateur est autorisé à supprimer l'adresse, si pas authorisé retourné le status unauthorized
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAdresse(@PathVariable Long id) {
+        Boolean isDeleted = adresseService.deleteAdresse(id);
+        HttpStatus status = (isDeleted) ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(status);
     }
 }
--- src/main/java/fr/polytech/Recruteur/services/AdresseService.java
+++ src/main/java/fr/polytech/Recruteur/services/AdresseService.java
@@ -3,6 +3,9 @@ package fr.polytech.Recruteur.services;
 
 import fr.polytech.Recruteur.models.Adresse;
 import fr.polytech.Recruteur.repositories.AdresseRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +17,48 @@ public class AdresseService {
 
     public Adresse findAdresseById(Long id) {
         return adresseRepository.findById(id).orElse(null);
+    }
+
+    public List<Adresse> findAllAdresses() {
+        return adresseRepository.findAll();
+    }
+
+    public Adresse addAdresse(Adresse adresse) {
+        if(adresse == null || adresse.getNumero_voie() == null || adresse.getNom_voie() == null 
+            || adresse.getCode_postal() == null || adresse.getVille() == null){
+            return null;
+        }
+        return adresseRepository.save(adresse);
+    }
+
+    public Adresse updateAdresse(Long idAdresse, Adresse adresse) {
+        Adresse adresseToUpdate = adresseRepository.findById(idAdresse).orElse(null);
+        if(adresseToUpdate == null){
+            return null;
+        }
+
+        if(adresse.getNumero_voie() != null){
+            adresseToUpdate.setNumero_voie(adresse.getNumero_voie());
+        }
+        if(adresse.getNom_voie() != null){
+            adresseToUpdate.setNom_voie(adresse.getNom_voie());
+        }
+        if(adresse.getCode_postal() != null){
+            adresseToUpdate.setCode_postal(adresse.getCode_postal());
+        }
+        if(adresse.getVille() != null){
+            adresseToUpdate.setVille(adresse.getVille());
+        }
+
+        return adresseRepository.save(adresseToUpdate);
+    }
+
+    public Boolean deleteAdresse(Long id) {
+        Boolean isDeleted = false;
+        if(adresseRepository.existsById(id)){
+            adresseRepository.deleteById(id);
+            isDeleted = true;
+        }
+        return isDeleted;
     }
 }
